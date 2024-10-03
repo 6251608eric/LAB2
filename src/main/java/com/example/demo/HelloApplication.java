5600823bd1d80ffb54751b54c87dfaf443ff7bc2
Eric <6251608@edu.vaniercollege.qc.ca>
2024-10-03 12:57:10 -0400

    Pushing the empty scenes that will house all different shapes

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,48 +1,18 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * Main Class where the primary menu scene and stage are set
- * @author Eric, Fadi, Daniel
- */
 public class HelloApplication extends Application {
-
-    public static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-
-        Label lbl1 = new Label("Main Screen");
-        lbl1.setFont(new Font("Arial", 20));
-        HBox hbox = new HBox(10);
-        VBox vbox = new VBox();
-        vbox.setSpacing(20);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(lbl1, hbox);
-        hbox.setAlignment(Pos.CENTER);
-
-        Button button = new Button("View SVG");
-        Button button2 = new Button("View Path Shape");
-        Button button3 = new Button("View Shear Transform");
-
-        hbox.getChildren().addAll(button, button2, button3);
-
-        scene = new Scene(vbox, 800, 400);
-        stage.setTitle("Main Screen");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
